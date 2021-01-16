f08fd9dd34babd1e5015e0e542a98ae5c1e81ea6
TopgunJHW <topgun_jian_hao@hotmail.com>
2021-01-16 11:55:39 +0100

    Replaced AccountBalance.css file with styled components

--- src/components/AccountBalance/AccountBalance.jsx
+++ src/components/AccountBalance/AccountBalance.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
-import './AccountBalance.css'
+import styled from 'styled-components';
+
+const Section = styled.section`
+    font-size: 2rem;
+    text-align: left;
+    padding: 1.5rem 0 1.5rem 5rem
+`;
 
 export default class AccountBalance extends Component {
     render() {
         return (
-            <section className="accountBalanceCSS">
-                ${this.props.amount}
-            </section>
+            <Section>
+                Balance: ${this.props.amount}
+            </Section>
         )
     }
 }
