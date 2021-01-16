f43b2c883e442d2c47529d1543787156365f505c
TopgunJHW <topgun_jian_hao@hotmail.com>
2021-01-16 11:09:40 +0100

    Added AccountBalance.css file

--- src/components/AccountBalance/AccountBalance.jsx
+++ src/components/AccountBalance/AccountBalance.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
+import './AccountBalance.css'
 
 export default class AccountBalance extends Component {
     render() {
         return (
-            <section>
+            <section className="accountBalanceCSS">
                 ${this.props.amount}
             </section>
         )
     }
 }
-
 
 AccountBalance.propTypes = {
     amount: propTypes.number.isRequired
